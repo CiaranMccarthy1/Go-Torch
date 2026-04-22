2e71be4c33dd151fc61175185bf4a971dfd25538
Ciaran <ciaranmccarthy100@gmail.com>
2026-04-22 23:06:45 +0100

    perf: optimize CPU matmul forward and backward

--- src/backend.go
+++ src/backend.go
@@ -5,6 +5,11 @@ import (
 	"sync"
 )
 
+const (
+	matMulBlockK = 64
+	matMulBlockN = 64
+)
+
 type TensorStorage interface {
 	Data() []float32
 }
@@ -52,46 +57,37 @@ func (b *CPUBackend) MatMulForward(a, bData []float32, m, k, n int) []float32 {
 	bT := b.Transpose(bData, k, n)
 	out := make([]float32, m*n)
 
-	numCPU := runtime.GOMAXPROCS(0)
-	chunkSize := (m + numCPU - 1) / numCPU
+	parallelFor(m, func(start, end int) {
+		for i := start; i < end; i++ {
+			aRow := a[i*k : (i+1)*k]
+			outRow := out[i*n : (i+1)*n]
 
-	var wg sync.WaitGroup
-	for w := 0; w < numCPU; w++ {
-		startRow := w * chunkSize
-		endRow := startRow + chunkSize
-		if startRow >= m {
-			break
-		}
-		if endRow > m {
-			endRow = m
-		}
+			for jBase := 0; jBase < n; jBase += matMulBlockN {
+				jEnd := minInt(jBase+matMulBlockN, n)
+				for kBase := 0; kBase < k; kBase += matMulBlockK {
+					kEnd := minInt(kBase+matMulBlockK, k)
+					for j := jBase; j < jEnd; j++ {
+						bRow := bT[j*k : (j+1)*k]
+						sum := outRow[j]
 
-		wg.Add(1)
-		go func(start, end int) {
-			defer wg.Done()
-			for i := start; i < end; i++ {
-				rowOffsetA := i * k
-				for j := 0; j < n; j++ {
-					rowOffsetB := j * k
-					var sum float32
+						x := kBase
+						for ; x <= kEnd-4; x += 4 {
+							sum += aRow[x] * bRow[x]
+							sum += aRow[x+1] * bRow[x+1]
+							sum += aRow[x+2] * bRow[x+2]
+							sum += aRow[x+3] * bRow[x+3]
+						}
+						for ; x < kEnd; x++ {
+							sum += aRow[x] * bRow[x]
+						}
 
-					x := 0
-					for ; x <= k-4; x += 4 {
-						sum += a[rowOffsetA+x] * bT[rowOffsetB+x]
-						sum += a[rowOffsetA+x+1] * bT[rowOffsetB+x+1]
-						sum += a[rowOffsetA+x+2] * bT[rowOffsetB+x+2]
-						sum += a[rowOffsetA+x+3] * bT[rowOffsetB+x+3]
+						outRow[j] = sum
 					}
-					for ; x < k; x++ {
-						sum += a[rowOffsetA+x] * bT[rowOffsetB+x]
-					}
-					out[i*n+j] = sum
 				}
 			}
-		}(startRow, endRow)
-	}
+		}
+	})
 
-	wg.Wait()
 	return out
 }
 
@@ -101,31 +97,120 @@ func (b *CPUBackend) MatMulBackward(a, bData, gradOut []float32, m, k, n int, ne
 
 	if needGradA {
 		gradA = make([]float32, m*k)
-		for i := 0; i < m; i++ {
-			for x := 0; x < k; x++ {
-				var sum float32
-				for j := 0; j < n; j++ {
-					sum += gradOut[i*n+j] * bData[x*n+j]
+
+		parallelFor(m, func(start, end int) {
+			for i := start; i < end; i++ {
+				gradARow := gradA[i*k : (i+1)*k]
+				gradOutRow := gradOut[i*n : (i+1)*n]
+
+				for kBase := 0; kBase < k; kBase += matMulBlockK {
+					kEnd := minInt(kBase+matMulBlockK, k)
+					for jBase := 0; jBase < n; jBase += matMulBlockN {
+						jEnd := minInt(jBase+matMulBlockN, n)
+						for x := kBase; x < kEnd; x++ {
+							bRow := bData[x*n : (x+1)*n]
+							sum := gradARow[x]
+
+							j := jBase
+							for ; j <= jEnd-4; j += 4 {
+								sum += gradOutRow[j] * bRow[j]
+								sum += gradOutRow[j+1] * bRow[j+1]
+								sum += gradOutRow[j+2] * bRow[j+2]
+								sum += gradOutRow[j+3] * bRow[j+3]
+							}
+							for ; j < jEnd; j++ {
+								sum += gradOutRow[j] * bRow[j]
+							}
+
+							gradARow[x] = sum
+						}
+					}
 				}
-				gradA[i*k+x] = sum
 			}
-		}
+		})
 	}
 
 	if needGradB {
 		gradB = make([]float32, k*n)
-		for x := 0; x < k; x++ {
-			for j := 0; j < n; j++ {
-				var sum float32
-				for i := 0; i < m; i++ {
-					sum += a[i*k+x] * gradOut[i*n+j]
+
+		aT := b.Transpose(a, m, k)
+		gradOutT := b.Transpose(gradOut, m, n)
+
+		parallelFor(k, func(start, end int) {
+			for x := start; x < end; x++ {
+				aCol := aT[x*m : (x+1)*m]
+				gradBRow := gradB[x*n : (x+1)*n]
+
+				for jBase := 0; jBase < n; jBase += matMulBlockN {
+					jEnd := minInt(jBase+matMulBlockN, n)
+					for j := jBase; j < jEnd; j++ {
+						gradOutCol := gradOutT[j*m : (j+1)*m]
+						var sum float32
+
+						i := 0
+						for ; i <= m-4; i += 4 {
+							sum += aCol[i] * gradOutCol[i]
+							sum += aCol[i+1] * gradOutCol[i+1]
+							sum += aCol[i+2] * gradOutCol[i+2]
+							sum += aCol[i+3] * gradOutCol[i+3]
+						}
+						for ; i < m; i++ {
+							sum += aCol[i] * gradOutCol[i]
+						}
+
+						gradBRow[j] = sum
+					}
 				}
-				gradB[x*n+j] = sum
 			}
-		}
+		})
 	}
 
 	return gradA, gradB
+}
+
+func minInt(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func parallelFor(total int, fn func(start, end int)) {
+	if total <= 0 {
+		return
+	}
+
+	if total < 32 {
+		fn(0, total)
+		return
+	}
+
+	workers := runtime.GOMAXPROCS(0)
+	if workers < 1 {
+		workers = 1
+	}
+	if workers > total {
+		workers = total
+	}
+
+	chunkSize := (total + workers - 1) / workers
+
+	var wg sync.WaitGroup
+	for worker := 0; worker < workers; worker++ {
+		start := worker * chunkSize
+		if start >= total {
+			break
+		}
+		end := minInt(start+chunkSize, total)
+
+		wg.Add(1)
+		go func(s, e int) {
+			defer wg.Done()
+			fn(s, e)
+		}(start, end)
+	}
+
+	wg.Wait()
 }
 
 var defaultBackend Backend = &CPUBackend{}
